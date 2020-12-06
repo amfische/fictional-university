a131b6e2d18799232f7ade477406293913bdb936
Aaron <amfischer21@gmail.com>
2020-12-06 10:00:45 -0500

    add wp-migrate-db plugin, removed config file from git tracking

--- wp-content/themes/fictional-university-theme/includes/like-route.php
+++ wp-content/themes/fictional-university-theme/includes/like-route.php
@@ -1,51 +1,65 @@
 <?php
 
-add_action( 'rest_api_init', 'universityLikeRoutes' );
+add_action( 'rest_api_init', 'university_like_routes' );
 
-function universityLikeRoutes() {
-	register_rest_route( 'university/v1', 'manage-like', [
-		'methods' => 'post',
-		'callback' => 'createLike',
-		'permission_callback' => '__return_true',
-	]);
+function university_like_routes() {
+	register_rest_route(
+		'university/v1',
+		'manage-like',
+		array(
+			'methods'             => 'post',
+			'callback'            => 'create_like',
+			'permission_callback' => '__return_true',
+		)
+	);
 
-	register_rest_route( 'university/v1', 'manage-like', [
-		'methods' => 'delete',
-		'callback' => 'deleteLike',
-		'permission_callback' => '__return_true',
-	]);
+	register_rest_route(
+		'university/v1',
+		'manage-like',
+		array(
+			'methods'             => 'delete',
+			'callback'            => 'delete_like',
+			'permission_callback' => '__return_true',
+		)
+	);
 }
 
-function createLike($data) {
-	if (!is_user_logged_in()) {
-		return wp_send_json_error(['msg' => 'You must be logged in to like a professor.'], 403);
+function create_like( $data ) {
+	if ( ! is_user_logged_in() ) {
+		return wp_send_json_error( array( 'msg' => 'You must be logged in to like a professor.' ), 403 );
 	}
 
-	if (get_post_status($data['professorId']) === false) {
-		return wp_send_json_error(['msg' => 'Professor does not exist.'], 404);
+	if ( get_post_status( $data['professorId'] ) === false ) {
+		return wp_send_json_error( array( 'msg' => 'Professor does not exist.' ), 404 );
 	}
 
-	$likes = get_field('user_likes', $data['professorId']) ?? [];
-	if (in_array(get_current_user_id(), $likes)) {
-		return wp_send_json_error(['msg' => 'You have already liked this professor.'], 422);
+	$likes = get_field( 'user_likes', $data['professorId'] ) ?? array();
+	if ( in_array( get_current_user_id(), $likes ) ) {
+		return wp_send_json_error( array( 'msg' => 'You have already liked this professor.' ), 422 );
 	}
 
 	$likes[] = get_current_user_id();
-	update_field('user_likes', $likes, $data['professorId']);
+	update_field( 'user_likes', $likes, $data['professorId'] );
 	return true;
 }
 
-function deleteLike($data) {
-	if (!is_user_logged_in()) {
-		return wp_send_json_error(['msg' => 'You must be logged in to do that.'], 403);
+function delete_like( $data ) {
+	if ( ! is_user_logged_in() ) {
+		return wp_send_json_error( array( 'msg' => 'You must be logged in to do that.' ), 403 );
 	}
 
-	$likes = get_field('user_likes', $data['professorId']) ?? [];
-	if (in_array(get_current_user_id(), $likes)) {
-		$likes = array_values(array_filter($likes, fn ($id) => $id !== get_current_user_id()));
-		update_field('user_likes', $likes, $data['professorId']);
+	$likes = get_field( 'user_likes', $data['professorId'] ) ?? array();
+	if ( in_array( get_current_user_id(), $likes, true ) ) {
+		$likes = array_filter(
+			$likes,
+			function( $id ) {
+				get_current_user_id() !== $id;
+			}
+		);
+		$likes = array_values( $likes );
+		update_field( 'user_likes', $likes, $data['professorId'] );
 		return true;
 	}
 
-	return wp_send_json_error(['msg' => 'Permission Denied.'], 422);
+	return wp_send_json_error( array( 'msg' => 'Permission Denied.' ), 422 );
 }
